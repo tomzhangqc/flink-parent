47b7bf8cf7bf823c2fc6f23997c06f87d95734db
zhangqingchun4 <zhangqingchun4@xdf.cn>
2021-01-26 16:35:43 +0800

    change

--- table-api/src/main/scala/table/demo/TableTest.scala
+++ table-api/src/main/scala/table/demo/TableTest.scala
@@ -1,37 +1,38 @@
 package table.demo
 
 
+import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.table.api.{Table, Tumble}
 import org.apache.flink.table.api._
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
 import org.apache.flink.types.Row
 import table.element.Test
+
+import java.time.Duration
 
 object TableTest {
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-    val inputDataStream: DataStream[String] = env.readTextFile("/Users/IdeaProjects/project/flink-demo/parent/table-api/src/main/resources/table.txt")
-    val lessonDataSet: DataStream[Test] = inputDataStream
+    val inputDataStream: DataStream[String] = env.readTextFile("/Users/winters/IdeaProjects/project/flink-parent/table-api/src/main/resources/table.txt")
+    val lessonDataSet: DataStream[Test] = (inputDataStream
       .map(data => {
         val words = data.split(" ")
         Test(words(0).toInt, words(1).toLong)
-      })
-      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[Test](Time.seconds(1)) {
-        override def extractTimestamp(element: Test): Long = {
-          element.time * 1000
-        }
-      })
-//    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
-//    val dataTable: Table = tableEnv.fromDataStream(lessonDataSet, 'id, 'time.rowtime as 'rowtime)
-//    val resultTable = dataTable.window(Tumble over 10.seconds on 'rowtime as 'w)
-//      .groupBy("w,id")
-//      .select("id,w.end")
-//    resultTable.toRetractStream[Row].print("stream table")
-//    dataTable.printSchema()
-    env.execute("stream word count job")
+      }) assignTimestampsAndWatermarks (WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(3)).withTimestampAssigner(new SerializableTimestampAssigner[Test]() {
+      override def extractTimestamp(element: Test, recordTimestamp: Long): Long = element.time * 1000
+    })))
+    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+    val dataTable: Table = tableEnv.fromDataStream(lessonDataSet, 'id, 'time.rowtime as 'rowtime)
+    val resultTable = dataTable.window(Tumble over 10.seconds on 'rowtime as 'w)
+      .groupBy('w, 'id)
+      .select('id, 'w.end)
+    val value = resultTable.execute().collect().forEachRemaining(println)
+    dataTable.printSchema()
+    env.execute()
   }
 }
